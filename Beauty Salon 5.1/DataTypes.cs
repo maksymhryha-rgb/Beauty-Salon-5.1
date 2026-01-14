03d3afee06804a5d00a5ed62d772514c482039bb
maksymhryha-rgb <maksym.hryha@student.uzhnu.edu.ua>
2026-01-14 08:33:48 +0200

    Багато виправлено

---
+++ Beauty Salon 5.1/DataTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace Beauty_Salon_5._1
+{
+    // Окремий файл для структур
+
+    struct Service
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+    }
+
+    // Структура для людей 
+    struct Person
+    {
+        public int Id;
+        public string Name;
+        public string Role; // "Client" або "Master"
+    }
+
+    // Структура запису на прийом
+    struct Appointment
+    {
+        public int Id;
+        public int ClientId;
+        public int StaffId;
+        public List<int> ServiceIds; // Список ID послуг
+        public DateTime Date;
+    }
+}
---
+++ Beauty Salon 5.1/SystemUtils.cs
@@ -0,0 +1,32 @@
+﻿namespace Beauty_Salon_5._1
+{
+    static class SystemUtils
+    {
+        public static void CheckAndCreate(string path, string header)
+        {
+            if (!File.Exists(path))
+            {
+                // Створюємо масив з одним рядком і записуємо
+                File.WriteAllLines(path, [header]);
+            }
+        }
+
+        public static int GetNextId(string path)
+        {
+            if (!File.Exists(path)) return 1; // Якщо файлу нема, то ID буде 1
+
+            var lines = File.ReadAllLines(path).Skip(1);
+            int max = 0;
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length > 0 && int.TryParse(parts[0], out int id))
+                {
+                    if (id > max) max = id;
+                }
+            }
+            return max + 1;
+        }
+    }
+}
